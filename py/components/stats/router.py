759ac9f52af9b770be7bff43fab5675e4793fc22
raj <rajsolan91@gmail.com>
2025-07-23 07:53:11 -0700

    Added in path to fetch stats data from historical

--- py/components/stats/router.py
+++ py/components/stats/router.py
@@ -10,8 +10,46 @@ from components.database import get_session
 from components.auth.utils import RBAChecker, ValidateJWT
 from fastapi.encoders import jsonable_encoder
 import pandas as pd
+from .schemas import StatsParams
+from components.symbols.models import Historical
 
 
 router = APIRouter()
 
+@router.post(
+    "/data",
+    dependencies=[
+        Depends(RBAChecker(roles=['admin', 'client'], permissions=None))]
+)
+async def get_data(
+    params: StatsParams,
+    session: AsyncSession = Depends(get_session),
+):
+    stmt = select(
+        Historical.id, 
+        Historical.custom_id, 
+        Historical.ticker_id, 
+        Historical.symbol,
+        Historical.milliseconds,
+        Historical.duration,
+        Historical.open,
+        Historical.low,
+        Historical.open,
+        Historical.close,
+        Historical.adj_close,
+        Historical.vwap,
+        Historical.timestamp,
+        Historical.transactions,
+        Historical.source,
+        Historical.market
+    ) \
+    .where(Historical.timestamp >= params.from_date) \
+    .where(Historical.timestamp <= params.to_date) \
+    .where(Historical.ticker_id == params.ticker_id) \
+    .where(Historical.source == params.source)
+    
+    results = await session.execute(stmt)
+    data    = results.mappings().all()
+    df      = pd.DataFrame(data)
+    return df.to_dict(orient='records')
 
--- py/components/stats/schemas.py
+++ py/components/stats/schemas.py
@@ -1,5 +1,13 @@
-from pydantic import BaseModel, field_validator
-from typing import Optional
-from enum import Enum
-from datetime import datetime
-from components.utils import decrypt, encrypt
+from pydantic import BaseModel, Field
+from typing import Optional, List, Any
+from datetime import date, timedelta
+
+
+class StatsParams(BaseModel):
+    ticker_id: int
+    from_date: date = date.today() - timedelta(days=100)
+    to_date: date = date.today()
+    indicators: List[dict[str, Any]] = Field(default_factory=
+        lambda: [{"name": "ma", "period": "50"}, {"name": "ma", "period": "200"}]
+    )
+    source: str = "POLYGON"
